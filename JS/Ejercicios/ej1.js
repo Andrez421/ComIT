71cf3ed73f3622acdbd24c15894ea6402bd5f115
Andrez421 <andres931204@gmail.com>
2025-01-15 19:10:14 -0500

    cambios

--- JS/Ejercicios/ej1.js
+++ JS/Ejercicios/ej1.js
@@ -1,7 +1,5 @@
-var userName = 'Andres';
-var lastName = 'Palacios';
-var age = 30;
-var dateOfBirth = '04/12/1993';
-var address= 'Carrera 4 # 1-40';
-
-console.log(userName, lastName, age, dateOfBirth, address);
+var userName
+var lastName
+var age
+var dateOfBirth
+var address
