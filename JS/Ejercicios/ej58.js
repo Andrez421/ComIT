b197be4fd46158dd8e0968dc9034973f0d03832c
Andrez421 <andres931204@gmail.com>
2025-01-17 19:56:06 -0500

    Ejercicos 58, 59 y 60 de JS

---
+++ JS/Ejercicios/ej58.js
@@ -0,0 +1,8 @@
+let hora = 7; 
+if (hora < 12 && hora > 5){
+    console.log("Buen día");
+} else if (hora <= 12 && hora > 5)  {
+    console.log("Buenas tardes");
+} else {
+    console.log("Buenas noches");
+}
---
+++ JS/Ejercicios/ej59.js
@@ -0,0 +1,9 @@
+let numero1 = 10;
+let numero2 = 8;
+if (numero1 == numero2) {
+  console.log('Los dos números son iguales');
+}else if (numero1 > numero2) {
+  console.log(`El numero ${numero1} es mayor que el numero ${numero2}`);
+}else { 
+  console.log(`El numero ${numero2} es mayor que el numero ${numero1}`);
+}
