719ce04ec65188d98ebcc5b0557427aef19f61c7
Andrez421 <andres931204@gmail.com>
2025-01-17 19:16:06 -0500

    Ejercicios 52, 53 y 54 de JS

---
+++ JS/Ejercicios/ej52.js
@@ -0,0 +1,8 @@
+const numero1 = 3;
+const numero2 = 5;
+if (numero1 > numero2) {
+  console.log("La variable numero1 es más grande que la variable numero2");
+}
+else {
+  console.log("La variable numero1 no es más grande que la variable numero2");
+}
---
+++ JS/Ejercicios/ej54.js
@@ -0,0 +1,7 @@
+let usuario = "pepe2018";
+let password = 12345;
+if (usuario == "pepe2017" && password == 12345) {
+    console.log("El usuario está autenticado y puede ver el sitio");
+}else{
+    console.log("Error, no se puede autenticar el usuario, puede intentarlo mas tarde");
+}
