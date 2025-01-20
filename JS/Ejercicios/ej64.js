3ed5813736e7a0071c30536976fe0db1fb56f2dc
Andrez421 <andres931204@gmail.com>
2025-01-20 18:42:30 -0500

    Ejercicios 64 y 65 de JS

---
+++ JS/Ejercicios/ej64.js
@@ -0,0 +1,13 @@
+let nombre = null;
+if (nombre) {
+  console.log("El nombre de usuario es:, " + nombre);
+}else{
+  console.log("Por favor, introduce un nombre de usuario");
+}
+
+let nombre2 = "Andres";
+if (nombre2) {
+  console.log("El nombre de usuario es:, " + nombre2);
+}else{
+  console.log("Por favor, introduce un nombre de usuario");
+}
---
+++ JS/Ejercicios/ej65.js
@@ -0,0 +1,11 @@
+const saldo = 0;
+if (saldo > 0) {
+  console.log(`El usuario tiene ${saldo} pesos a favor.`);
+}else 
+    console.log(`El usuario no tiene saldo a favor.`);
+
+const saldo2 = 100;
+if (saldo2 > 0) {
+  console.log(`El usuario tiene ${saldo2} pesos a favor.`);
+}else 
+    console.log(`El usuario no tiene saldo a favor.`);
