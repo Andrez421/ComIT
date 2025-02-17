287dda03e1f962e6369989eaec517f4482d8b458
Andrez421 <andres931204@gmail.com>
2025-02-17 12:07:06 -0500

    test

---
+++ Jest/ej1.js
@@ -0,0 +1,9 @@
+describe("Nuestro primer test", () => {
+    test("las variables estan definidas", () => {
+        expect(nombre).toBeDefined();
+        expect(apellido).toBeDefined();
+        expect(edad).toBeDefined();
+        expect(fechadenacimiento).toBeDefined();
+        expecto(direccion).toBeDefined();
+    });
+    
