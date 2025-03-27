a5e3d78521042f4a0d979dd6826d53c8df052e7e
Andrez421 <andres931204@gmail.com>
2025-03-26 19:35:05 -0500

    estructura básica de navegación en la aplicación React Native

---
+++ React Native/navegacion/app/about.tsx
@@ -0,0 +1,27 @@
+import { View, StyleSheet, Button } from "react-native";
+import { Link, router } from "expo-router";
+
+export default function About() {
+  const volver = () => {
+    if (router.canGoBack()) {
+      router.back();
+    } else {
+      router.navigate("/");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Link href="/">Navegar a Home</Link>
+      <Button title="Volver a pantalla anterior" onPress={volver} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
---
+++ React Native/navegacion/app/index.tsx
@@ -0,0 +1,20 @@
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Link href="/about" dismissTo>
+        Navegar a About
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
