09c862b58b09e7a66813632ac21018a4baae10f6
Andrez421 <andres931204@gmail.com>
2025-03-28 22:20:55 -0500

    build: add remote-redux-devtools dependency and update store configuration
    Add remote-redux-devtools package to enhance Redux debugging capabilities in development. Update store.ts to configure middleware and enhancers for better serialization and debugging support.

--- React Native/redux/redux/store.ts
+++ React Native/redux/redux/store.ts
@@ -1,32 +1,29 @@
 import { configureStore } from "@reduxjs/toolkit";
 import devToolsEnhancer from "redux-devtools-expo-dev-plugin";
+
 import counterReducer from "./counterSlice";
 
-// Configuración del enhancer con opciones correctas
-const enhancer = devToolsEnhancer({
-  name: "React Native Redux",
-  realtime: true,
-  trace: true
-});
+export type RootState = ReturnType<typeof store.getState>;
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
-  devTools: __DEV__, // Usamos __DEV__ para asegurarnos que solo se active en desarrollo
-  enhancers: (getDefaultEnhancers) => {
-    if (__DEV__) {
-      return getDefaultEnhancers().concat(enhancer);
-    }
-    return getDefaultEnhancers();
-  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+  enhancers: (getDefaultEnhancers) =>
+    __DEV__
+      ? getDefaultEnhancers().concat(
+          devToolsEnhancer({
+            name: "Redux DevTools",
+            hostname: "localhost",
+            port: 8081,
+            realtime: true,
+            secure: false,
+            suppressConnectErrors: false
+          })
+        )
+      : getDefaultEnhancers(),
 });
-
-export type RootState = ReturnType<typeof store.getState>;
-
-// Agregamos logging para desarrollo
-if (__DEV__) {
-  store.subscribe(() => {
-    console.log("Estado actualizado:", store.getState());
-  });
-}
